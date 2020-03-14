eb96a4926f1c48f37660c1aa04e7b0ff70061feb
ranoualachheb <ranoua22@gmail.com>
2020-03-14 10:22:30 +0100

    modification done

--- warmUp2.js
+++ warmUp2.js
@@ -1,18 +1,28 @@
 // 1 - Complete the function cube that returns the cube of x:
 
 function cube(x) {
+	return x*x*x ;
   // your code here
 }
 
 // 2 - Write a function sameLength that accepts two strings as arguments, and returns true if those strings have the same length, and false otherwise.
-
-function sameLength(string1, string2) {
-  // your code here
+function sameLength(str1,str2){
+	if (str1.length === str2.length){
+		return true ; 
+	}
+	    return false ;
 }
 
 // 3 - Write a function called scoreToGrade that accepts a number as a parameter and returns a string representing a letter grade corresponding to that score.
 
 function scoreToGrade(number) {
+	if (number === 0 ) {
+		return 'F';
+	} else if (number < 60 ){
+		return 'B' ;
+	} else {
+		return 'A' ;
+	}
   // your code here
 }
 // USE RECURSION
@@ -22,6 +32,11 @@ function scoreToGrade(number) {
 // repeatString('dog', 2); // => 'dog' + 'dog' => 'dogdog'
 // repeatString('dog', 3); // => 'dog' + 'dog' + 'dog' => 'dogdogdog'
 
-function repeatString(str, count) {
+function repeatString(str, count) { 
+	var string = ''
+	if ( count === 0){
+		return string ;
+	}
+	return string = str + repeatString(str, count-1)
   // TODO: your code here
 }
