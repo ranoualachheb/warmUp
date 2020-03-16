39d883a3be3b607b388ed25e0c7dbf45e1b0fdc1
ranoualachheb <ranoua22@gmail.com>
2020-03-16 09:52:50 +0100

    solved

--- warmUp3.js
+++ warmUp3.js
@@ -1,8 +1,14 @@
 // 1 - Write functions larger and smaller that each accept two strings as arguments, and return the larger and smaller strings, respectively
-
+function larsma(str1,str2){
+if (str1.length> str2.length){
+	return str1 + ' '+ str2
+}
+}
 // 2 - Write a function called counter that, when invoked, always returns a number that is one more than the previous invocation. For instance:
 // function counting(n) { 
-    TODO: your code here 
+    function counter(n){
+    	return n+1
+    }
 // } 
 // counting(5); // => '1, 2, 3, 4, 5' 
 // counting(1); // => '1' 
@@ -10,6 +16,8 @@
 
 
 // 3 -  Meet & Greet: n numbers are coming to a party for numbers. Introduce them each to all the guests as they arrive. You should end up with output looking something like this one for n = 5
+i saw the solution for this exercice and i still didnt get it.. sorry :p 
+
 // welcome 1
 //  welcome 2, meet 1
 //  welcome 3, meet 1 and 2
@@ -18,4 +26,11 @@
 //
 
 // 4- Write a function shortestWord that works like longestWord, but returns the shortest word instead.
+function shortestword(str,str1){
+	var total = ''
+	if (str.length < str1.length){
+		total = total + str
+	}
+	return total ; 
+}
 
