540a244d58cbba40aa1f08daa3a5e7fea9b2ca43
ranoua lachheb <ranoua22@gmail.com>
2020-03-23 10:46:54 +0100

    warm up 9 done

--- warmUp9.js
+++ warmUp9.js
@@ -4,6 +4,12 @@
 //1. Write a JavaScript function to check whether an `input` is an array or not
 // ex isArray([1,2])=>true
 //    isArray(5)=>false
+// i found this function in mdn
+function isArray(arr){
+	return array.isArray(arr)
+}
+
+
 
 // **************** git status ****************
 // **************** git add fileName.js ****************
@@ -15,6 +21,14 @@
 //2-Write a JavaScript function to get the first n element of an array.
 //ex  first([1,2,3],1)=>[1]
 //    first([1,2,3,4]2)=>[1,2]
+// SO i put i<n as a stop condition so whenever i reaches the n element it stops.. before doing so it pushes every number it loops on which is less than n in the new array1 ad then returns it 
+function first(arr,n){
+	 var array1=[]
+	for (var i=0 ; i<n; i++){
+		array1.push(arr[i])
+	}
+	return array1
+}
 
 // **************** git status ****************
 // **************** git add fileName.js ****************
@@ -25,10 +39,16 @@
 
 //3- Write a JavaScript function to convert an object into a list of `[key, value]` pairs
 //ex: convert({name:'ahmed',age:20})=>[['name','ahmed'],['age', 20]]
+function javascript(obj){
 
+}
 // **************** git status ****************
 // **************** git add fileName.js ****************
 // **************** git status ****************
 // **************** git commit -m "your message" ****************
 // **************** git status ****************
 // **************** git push origin master ****************
+
+
+
+// i can do this i just need more time 
